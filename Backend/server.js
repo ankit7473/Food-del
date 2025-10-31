a335850109a3a42ab31fcad8cd81c3f2589a4819
Ankit kumar <ankitkumar62601@gmail.com>
2025-11-01 04:19:42 +0530

    Change port configuration to use environment variable

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ import orderRouter from "./routes/orderRoute.js"
 
 //app config
 const app=express()
-const port= "https://food-del-339z.onrender.com";
+const port= process.env.PORT || 4000;
 
 
 // middleware
